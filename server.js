3979a98fd564fe8f086fd4eebac473f294582308
Cris Franco <cris.franco219@gmail.com>
2021-03-11 13:54:32 -0500

    added connection to mongodb and required routes file to server.js

--- server.js
+++ server.js
@@ -15,6 +15,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// DB Mongo connection
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
 // Run server
 app.listen(PORT, function () {
   console.log(`App listening on Port ${PORT}!`);
