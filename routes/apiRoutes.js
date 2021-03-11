63afd7ab5845400d674d4ea23b54687d393b2435
Cris Franco <cris.franco219@gmail.com>
2021-03-11 14:06:41 -0500

    created a put route to update the workouts page using the id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -32,4 +32,17 @@ module.exports = function (app) {
         res.status(400).json(err);
       });
   });
+  // Update the workouts page using id value
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+      { _id: req.params.id },
+      { exercises: req.body }
+    )
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
 };
